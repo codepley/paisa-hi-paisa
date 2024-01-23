139e4dce9154522a35c408791c016df8601e4149
codepley <officialkushalkaran@gmail.com>
2024-01-24 01:59:44 +0530

    added cors and jsonwebtoken dependency

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,15 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+// routes
+import rootRouter from './routes/index.js'
+
+app.use('/api/v1', rootRouter)
 
 export {app}
